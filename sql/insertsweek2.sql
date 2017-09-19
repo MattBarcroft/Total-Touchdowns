6eaacb8af85a7021e5aea92b73ec6ed2024de13d
Mandy Thornton <mth01@sbgml02461.sbg.skybettingandgaming.com>
2017-09-19 11:27:18 +0100

    added insertsweek2.sql in sql folder

---
+++ sql/insertsweek2.sql
@@ -0,0 +1,57 @@
+INSERT INTO games
+(
+
+  	hometeam_id,
+  	awayteam_id,
+  	hometeamactualscore,
+  	awayteamactualscore,
+  	kickoff_datetime,
+  	location,
+  	week_id
+)
+
+VALUES (
+    "2",
+    "9",
+    NULL,
+    NULL,
+    "2017-09-24 13:00:00",
+    "Atlanta",
+    "2"),
+
+(
+    "4",
+    "7",
+    NULL,
+    NULL,
+    "2017-09-24 13:00:00",
+    "Buffalo",
+    "2"),
+
+    (
+    "8",
+    "5",
+    NULL,
+    NULL,
+    "2017-09-24 13:00:00",
+    "Denver",
+    "2"),
+
+    (
+    "6",
+    "3",
+    NULL,
+    NULL,
+    "2017-09-24 13:00:00",
+    "Cincinnati",
+    "2"),
+
+    (
+    "10",
+    "1",
+    NULL,
+    NULL,
+    "2017-09-24 13:00:00",
+    "Miami",
+    "2")
+    ;
