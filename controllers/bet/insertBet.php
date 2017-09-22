5dda40adc22ef6b3152738132a8e3eb912702ce3
Matthew Barcroft <mba01@sbgml03152.sbg.skybettingandgaming.com>
2017-09-22 15:21:55 +0100

    altered games page

--- controllers/bet/insertBet.php
+++ controllers/bet/insertBet.php
@@ -1,14 +1,18 @@
 <?php
 include("../../includes/init.php");
 
+
 var_dump($_POST);
+echo "<br><br>";
 
+$db = get_db();
 
+//$pdo = $db->execute()
+//insert row into bet, get user id by session
+for ($i=1; $i <= count($_POST); $i++) {
+  echo "gameid = $i";
+  echo $_POST["htscore-game-$i"];
+  echo $_POST["atscore-game-$i"];
+}
 
-$bet = new bet(
-  $bet;
-);
-
-
-
-header("Location: /games/index.php");
+//header("Location: /games/index.php");
